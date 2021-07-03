a19e3acf7b21056981659b9b61eaf2c2acfb4101
Hiron68k <hiron.de.dosukoi@gmail.com>
2021-07-04 04:44:30 +0900

    add jar artifact

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,36 @@
-
 import java.io.File
+import java.io.FilenameFilter
 import java.util.*
 
 fun main(args: Array<String>) {
-    fileList(".")
+    //引数で渡されたディレクトリのファイルリストを取得する
+    val dirList = fileList(args[0])
+    println("余計なファイルがないこと、順序が正しいことを確認してください")
+    print("よろしいですか?(yes/ ) ")
+    if(readLine().equals("yes") ) {
+        //実行する
+        println("executing...")
+    } else {
+        //何もしないで終了する
+        return
+    }
 }
 
+/// fileList
+/// 引数で渡されたディレクトリのファイル一覧を返す
+/// ただし、tree.txtは除外する
 fun fileList(dirName: String) : Array<String> {
+
+    val filter = FilenameFilter { _, str -> //指定文字列でフィルタする
+        //`tree.txt`は除外する`
+        str.indexOf("tree.txt") == -1
+    }
+
     // ファイル一覧を取得するディレクトリ
     val dir = File(dirName)
 
     //Fileクラスのlistメソッドを使用してファイル一覧を取得する
-    val fileList = dir.list() ?: return arrayOf("")
+    val fileList = dir.list(filter) ?: return arrayOf("")
     Arrays.sort(fileList)
 
     // 取得したファイル一覧を表示する
